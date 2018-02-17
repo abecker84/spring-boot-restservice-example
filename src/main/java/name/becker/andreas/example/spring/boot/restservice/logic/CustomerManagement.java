abec4acaf909f46f8eb5a1a74518e6a3437e4774
Andreas Becker <andreas@becker.name>
2018-02-17 12:31:39 +0100

    Added GPLv3 copyleft information

--- src/main/java/name/becker/andreas/example/spring/boot/restservice/logic/CustomerManagement.java
+++ src/main/java/name/becker/andreas/example/spring/boot/restservice/logic/CustomerManagement.java
@@ -2,6 +2,24 @@ package name.becker.andreas.example.spring.boot.restservice.logic;
 
 import java.util.List;
 
+/*
+ * Copyright 2018, Andreas Becker <andreas AT becker DOT name>
+ * 
+ * This file is part of The Spring Boot REST Service example.
+ * 
+ * The Spring Boot REST Service example is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * The Spring Boot REST Service example is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with The Spring Boot REST Service example. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Component interface of the customer management component.
  */
